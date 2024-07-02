6707cf0ea55156f9738408fddc7e6c878c121a5c
Jesus Garcia <jgarciajr45@gmail.com>
2024-07-02 11:48:10 -0700

    RonYe

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+
+            string ronSwansonUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            string kanyeWestUrl = "https://api.kanye.rest";
+            int numberOfQuotes = 5;
+
+            HttpClient client = new HttpClient();
+
+            for (int i = 0; i < numberOfQuotes; i++)
+            {
+                // Get Ron Swanson quote
+                var ronSwansonResponse = client.GetStringAsync(ronSwansonUrl).Result;
+                JArray ronSwansonQuoteArray = JArray.Parse(ronSwansonResponse);
+                string ronSwansonQuote = ronSwansonQuoteArray[0].ToString();
+
+                // Get Kanye West quote
+                var kanyeWestResponse = client.GetStringAsync(kanyeWestUrl).Result;
+                JObject kanyeWestQuoteObject = JObject.Parse(kanyeWestResponse);
+                string kanyeWestQuote = kanyeWestQuoteObject["quote"].ToString();
+
+                // Print the quotes
+                Console.WriteLine($"Ron Swanson: {ronSwansonQuote}");
+                Console.WriteLine($"Kanye West: {kanyeWestQuote}");
+                Console.WriteLine();
+
+            }
+
+
         }
     }
 }
